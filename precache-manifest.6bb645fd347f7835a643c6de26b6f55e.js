b6b64fa4a30667932dc3421d33ec631f62f480c5
git-blue-white <104221657+git-blue-white@users.noreply.github.com>
2022-05-03 10:42:10 -0700

    Updates

---
+++ precache-manifest.6bb645fd347f7835a643c6de26b6f55e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b96614de16a8c9186ef",
+    "url": "/static/css/main.d88981be.chunk.css"
+  },
+  {
+    "revision": "0b96614de16a8c9186ef",
+    "url": "/static/js/main.0b96614d.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c89bfc22de0455b6b2610f36bd4a217",
+    "url": "/index.html"
+  }
+];
